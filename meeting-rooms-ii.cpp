57389b3053e7d599c1611bf0ae772bee1c9ca1d5
Nishanth Gaddam <nishanth.gaddam37@gmail.com>
2020-12-23 01:57:39 -0600

    Completed prob 1

---
+++ meeting-rooms-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        if(intervals.size() == 0) return 0;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        sort(intervals.begin(), intervals.end());
+        pq.push(intervals[0][1]);
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0] >= pq.top()) {
+                pq.pop();
+            }
+            pq.push(intervals[i][1]);
+        }
+        return pq.size();
+    }
+};
