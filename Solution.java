4c72a3653dc52b7304bcf4c341f420d7a6322486
tholerohit <76415875+tholerohit@users.noreply.github.com>
2021-02-11 19:54:35 -0500

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,18 @@
+//Time Complexity: m*n(logk)
+//Space Complexity: O(k)
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+
+                pq.add(matrix[i][j]);
+                if(pq.size() > k){
+                    pq.poll();
+                }
+            }
+        }
+    return pq.poll();
+    }
+}
