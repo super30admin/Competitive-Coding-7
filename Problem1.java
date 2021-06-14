b65d87c07a32375e37d709e0b4a03f03dfa269ef
Deepikasri5 <deepikasrigr.1995@gmail.com>
2021-06-14 01:23:53 -0400

    CP-7

---
+++ Problem1.java
@@ -0,0 +1,35 @@
+//Time Complexity:O(n+logn)
+//Space Complexity:O(1)
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n =matrix.length-1;
+        int low=matrix[0][0];
+        int high=matrix[n][n];
+        while(low<high){
+            int pivot=low+(high-low)/2;
+            if(getCount(matrix,pivot)<k){
+                low=pivot+1;
+            }else{
+                high=pivot;
+            }
+        }
+        return high;
+    }
+    private int getCount(int[][] matrix, int piv){
+
+        int n =matrix.length;
+        //row will be first row
+        int r= 0;
+        //last coloumn
+        int c=n-1;
+        int count=0;
+        while(r<=n-1 && c>=0){
+            if(matrix[r][c]<=piv){
+                count =count+c+1;
+                r++;
+            }
+            else c--;
+        }
+        return count;
+    }
+} 
---
+++ Problem2.java
@@ -0,0 +1,16 @@
+//Time Complexity:O(nlogn)
+//Space Complexity:O(n)
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(-1);
+        for(int i=0;i<intervals.length;i++){
+            if(pq.peek()<=intervals[i][0]){
+                pq.poll();
+            }
+            pq.add(intervals[i][1]);
+        }
+        return pq.size();
+    }
+} 
