5504c5ed594ae28d1de9af2ed55c38069b4596b7
Durgesh Patil <patildurgesh1998@gmail.com>
2021-09-07 13:19:35 -0400

    Update Problem-2.cpp

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -10,7 +10,7 @@ public:
         return a[0]<b[0];
     }
     int minMeetingRooms(vector<vector<int>> intervals) {
-        sort(intervals.begin(), intervals.end(), cmp);
+        sort(intervals.begin(), intervals.end(), cmp);                      //we are sorting the intervals on the basis of its starting time, so as when we insert the ending time of the meeting in the priority queue(min heap). As we compare the top of the priority queue with the next ending time of the meeting in the intervals.
         priority_queue<int, vector<int>, greater<int>> pq;
         for(auto interval : intervals){
             if(pq.size() > 0 && pq.top() <= interval[0]){
