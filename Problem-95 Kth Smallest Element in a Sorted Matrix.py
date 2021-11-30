88340c3c5e2bb20bc10025e369e867e6a6b17ff7
Adhish Shrivastava <adhish70@gmail.com>
2021-11-30 11:34:10 -0500

    Problem96 done

---
+++ Problem-95 Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,11 @@
+# 378. Kth Smallest Element in a Sorted Matrix
+# https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
+# Logic: 
+
+# Time Complexity:
+# Space Complesxity: 
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        pass
