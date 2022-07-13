98701bb2d42bc3e902ec42b38e092be92bdeea1e
Sahithi Kamireddy <sahithi.kamireddy@gmail.com>
2022-07-13 16:40:55 -0700

    Completed Competitive-Coding-7

---
+++ KthSmallestElementInSortedMatrix.java
@@ -0,0 +1,26 @@
+/* Time Complexity :O(nlogk)
+ * Space Complexity :O(k)
+ * Did this code successfully run on Leetcode :yes
+ * Any problem you faced while coding this :No
+ */
+
+import java.util.*;
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        // null case
+        if(matrix == null || matrix.length == 0) return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                maxHeap.add(matrix[i][j]);
+                if(maxHeap.size() > k) {
+                    maxHeap.poll();
+                }
+            }
+        }
+        return maxHeap.poll();
+    }
+}
