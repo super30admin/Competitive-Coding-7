dadaf3fd484e7c7b27b477b367535cc5f23667ff
Manthan Wadiwala <manthancar@gmail.com>
2023-03-20 12:15:35 -0400

    Completed CC-7

---
+++ MeetingRoomsII.java
@@ -0,0 +1,17 @@
+// TC = O(nlogm)
+// SC = O(n)
+
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int [] interval: intervals){
+            if(!pq.isEmpty() && pq.peek() <= interval[0]){
+                pq.poll();
+            }
+            pq.add(interval[1]);
+        }
+        return pq.size();
+    }
+}
