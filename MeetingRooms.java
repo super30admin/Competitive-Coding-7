e5f2ca84b0081b1040a315b93057a1a4d460bd8f
sangram vuppala <vuppala.s@northeastern.edu>
2023-07-10 00:08:39 +0530

    Solution updated

--- MeetingRooms.java
+++ MeetingRooms.java
@@ -4,19 +4,18 @@ class Solution {
     public int minMeetingRooms(int[][] intervals) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         //sort
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int count = 1;
-        minHeap.add(intervals[0][1]);
+        Arrays.sort(intervals, (a, b) -> a[0]- b[0]);
+
         //compare with adjacent rooms and add to heap
-        for(int i = 1; i< intervals.length; i++){
-            if(minHeap.peek() <= intervals[i][0]){
+        for(int i = 0; i< intervals.length; i++){
+            if(!minHeap.isEmpty() && minHeap.peek() <= intervals[i][0]){
                 minHeap.poll();
                 minHeap.add(intervals[i][1]);
             }else{
                 minHeap.add(intervals[i][1]);
-                count++;
+
             }
         }
-        return count;
+        return minHeap.size();
     }
 }
