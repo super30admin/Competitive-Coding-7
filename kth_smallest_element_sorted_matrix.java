7154cbf6f8cb1808dcceecba76f80c910b914806
SINDHURA_PARUCHURI <paruchuri.sindhu13@gmail.com>
2024-03-10 21:49:01 -0700

    Competitive coding 7

---
+++ kth_smallest_element_sorted_matrix.java
@@ -0,0 +1,16 @@
+
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+     PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> b-a);
+     for(int i =0; i<matrix.length;i++) {
+         for(int j=0; j<matrix[0].length;j++ ) {
+             pq.add(matrix[i][j]);
+             while(pq.size() > k) {
+                pq.poll();
+             }
+         }
+     }
+     return pq.peek();   
+    }
+}
